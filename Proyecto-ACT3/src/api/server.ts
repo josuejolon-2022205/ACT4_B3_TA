c4bccc8bd606a9e31affe8ed2a6cc4f2ead87eb7
josuejolon-2022205 <jjolon-2022205@kinal.edu.gt>
2026-07-15 11:25:43 -0600

    feat: se ha creado el server para almacenar la ruta y el puerto

---
+++ Proyecto-ACT3/src/api/server.ts
@@ -0,0 +1,12 @@
+import * as http from "http";
+import { routerProducto } from "./router"; 
+
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+    await routerProducto(req, res);
+});
+
+server.listen(port, () => {
+    console.log(`Servidor en http://localhost:${port}`);
+});
